bdcb6f1d19a32947ddf3e2bbacb748b695274ed3
maitereynoso <maitereynosov@gmail.com>
2026-06-09 11:10:31 -0300

    delete: html and js

--- src/app.js
+++ src/app.js
@@ -6,16 +6,12 @@ import { AuthRouter } from './routes/authRouter.js'
 import { ArticleRouter } from './routes/articleRouter.js'
 import { connectDB } from './config/mongoDBConnection.js'
 
-try {
-    process.loadEnvFile() // Reads the env variables on .env file (used for dev).
-} catch (error) {
-    config()
-}
+config()
+process.loadEnvFile() // Reads the env variables on .env file (used for dev).
 
 const server = express()
 server.use(express.json()) // Post request can be read in json.
 server.use(cors())
-server.use(express.static('public'))
 
 const environment = 'dev'
 let port
