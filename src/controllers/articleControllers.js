2e96c7dfca30ee9faf4ee2377354b403dcf9f1a6
maitereynoso <maitereynosov@gmail.com>
2026-06-29 15:10:19 -0300

    fix limit on query params

--- src/controllers/articleControllers.js
+++ src/controllers/articleControllers.js
@@ -4,7 +4,7 @@ import { articleSchema } from '../services/articleValidations.js'
 const getArticles = async (req, res) => {
     try {
         const userLogged = req.userLogged // 'userLogged' is a req property made on the middleware. It contains the data from the user.
-        const limit = Number(req.query.limit) || 1 // By default the limit is 10 elements.
+        const limit = Number(req.query.limit) || 4 // By default the limit is 10 elements.
         const page = Number(req.query.page) || 1
         const { category, userId, sort, order } = req.query // Filters
         const skip = (page - 1) * limit
